f46cb71ac45e1b7b0e26b872fb8b8552bab8e192
leomozzer <mozzer.ferraz@gmail.com>
2024-11-30 14:35:54 -0300

    Testing workflow

--- terraform-live/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-lsoavdstac"
-    storage_account_name = "lsoavdstac"
-    container_name       = "states"
-    key                  = "prod.tfstate"
-  }
-}
--- terraform-live/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  skip_provider_registration = true
-  subscription_id            = var.avd_subscription_id
-  features {
-
-  }
-}
-
